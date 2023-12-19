00c1c7cfb63dcd71bf344062b09e77ac3808e146
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-19 21:10:32 +0530

    edit

--- src/components/s_plant_facilities/S_PlantFacilitiesComponent.js
+++ src/components/s_plant_facilities/S_PlantFacilitiesComponent.js
@@ -4,11 +4,13 @@ import { FaArrowLeft } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
 import { BsThreeDots } from "react-icons/bs";
 import { MdDeleteOutline } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 
 const SPlantFacilitiesComponent = () => {
     const [showAddModal, setShowAddModal] = useState(false);
     const [showDropDownSPlantSFac, setShowDropDownSPlantSFac] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showEditSPlantSFacModal, setShowEditSPlantSFacModal] = useState(false);
 
     
     const handlePlusSPlantSFacClick = () => {
@@ -223,6 +225,120 @@ const SPlantFacilitiesComponent = () => {
                                                         onClick={() => setShowDeleteModal(false)}
                                                         >
                                                         Yes
+                                                        </button>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                                </div>
+                                            </>
+                                            ) : null}
+
+                                        <p className="m-0 text-white whitespace-nowrap ml-[90px] mt-3 cursor-pointer" onClick={() => setShowEditSPlantSFacModal(true)}><CiEdit/></p>
+                                        {showEditSPlantSFacModal ? (
+                                            <>
+                                            <div className="bg-slate-900">
+                                                <div
+                                                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                                >
+                                                <div className="relative my-6 mx-auto w-[800px]">
+
+                                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-green-50 outline-none focus:outline-none">
+                                                
+                                                    <div className="flex items-start justify-between p-5 ">
+                                                        <h3 className="text-3xl font-semibold text-black">
+                                                        Edit 
+                                                        </h3>
+                                                        <button
+                                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                        onClick={() => setShowEditSPlantSFacModal(false)}
+                                                        >
+                                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                            ×
+                                                        </span>
+                                                        </button>
+                                                    </div>
+                                                    <div className="mb-4">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-680px]">
+                                                        Location:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="location"
+                                                        name="location"
+                                                        //value={location}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4 mt-2">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-660px]">
+                                                        Description:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="description"
+                                                        name="description"
+                                                        //value={description}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4 mt-2">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-690px]">
+                                                        Parent:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="parent"
+                                                        name="parent"
+                                                        //value={parent}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4 mt-2">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-690px]">
+                                                        System:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="system"
+                                                        name="system"
+                                                        //value={system}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4 mt-2">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-700px]">
+                                                        Level:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="level"
+                                                        name="level"
+                                                        //value={level}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                    
+                                                    <div className="flex items-center justify-end p-6">
+                                                        <button
+                                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-10" 
+                                                        type="button"
+                                                        onClick={() => setShowEditSPlantSFacModal(false)}
+                                                        >
+                                                        Cancel
+                                                        </button>
+                                                        <button
+                                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                        onClick={() => setShowEditSPlantSFacModal(false)}
+                                                        >
+                                                        Update
                                                         </button>
                                                     </div>
                                                     </div>
