f9ec78a88368dd2f774441c1dfa538fe0dc01c28
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-19 18:56:34 +0530

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,27 @@
 import React from 'react'
 // import './Navbar.scss'
+import { FaCog, FaUser } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <nav className="bg-gray-800 p-4 mb-4 text-white rounded-lg">
-            {/* Navbar content goes here */}
-            Navbar
+        <nav className="bg-white text-black rounded-lg h-12 mb-3 ">
+            <div className='text-lg text-darkslategray-100 font-nunito-sans flex '>
+            <div className="rounded-[14px] bg-white shadow-[0px_6px_58px_rgba(196,_203,_214,_0.1)]" />
+            
+            <input
+                type="text"
+                placeholder="Search"
+                className="rounded border-none outline-none bg-slate-200 mt-2 ml-2 w-[50%]"
+                
+            />
+            
+            <div className="cursor-pointer flex items-end ml-[42%]">
+                <FaCog size={20} />
+            </div>
+            <div className="cursor-pointer flex items-end ml-[2%]">
+                <FaUser size={20} />
+            </div>
+            </div>
         </nav>
     )
 }
