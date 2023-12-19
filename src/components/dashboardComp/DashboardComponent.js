478b94f626641e81c4f5eb2db4747b3a3626857b
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-19 18:30:28 +0530

    delete modal

--- src/components/dashboardComp/DashboardComponent.js
+++ src/components/dashboardComp/DashboardComponent.js
@@ -14,6 +14,7 @@ const DashboardComponent = () => {
     const [showDropdown, setShowDropdown] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [showEditModal, setShowEditModal] = useState(false);
+    const [showDropDownSPlantSFac, setShowDropDownSPlantSFac] = useState(false);
     
     const handleSRegionClick = () => {
         setShowSRegion(!showSRegion);
@@ -25,6 +26,11 @@ const DashboardComponent = () => {
     const handlePlusClick = () => {
         setShowDropdown(!showDropdown);
       };
+
+      const handlePlusSPlantSFacClick = () => {
+        console.log("ssss");
+        setShowDropDownSPlantSFac(!showDropDownSPlantSFac)
+      }
 
     return (
         <div className="bg-white p-4 h-[87.1vh] rounded-lg shadow-md">
@@ -179,6 +185,7 @@ const DashboardComponent = () => {
 
                                 <div className='flex gap-10'>
                                 <div className="rounded-xl bg-cornflowerblue  overflow-hidden shrink-0 w-[25%] bg-blue-800 ml-20 mt-5">
+                                    
                                     <div className=" font-semibold text-[15px]">
 
 
@@ -196,8 +203,8 @@ const DashboardComponent = () => {
 
                                         {!showDropdown && (
                                             <>
-                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - ADMIN`}</p>
-                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S - Adminstration`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - FIELD`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S Fields`}</p>
                                             </>
                                         )}
                                     </div>
@@ -208,8 +215,8 @@ const DashboardComponent = () => {
 
                                         {!showDropdown && (
                                             <>
-                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - ADMIN`}</p>
-                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S - Adminstration`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - GEN`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S General`}</p>
                                             </>
                                         )}
                                     </div>
@@ -223,20 +230,78 @@ const DashboardComponent = () => {
 
                                         {!showDropdown && (
                                             <>
-                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - ADMIN`}</p>
-                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S - Adminstration`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - PIP`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S Pipelines`}</p>
                                             </>
                                         )}
                                     </div>
                                 </div>
                                 <div className="rounded-xl bg-cornflowerblue  overflow-hidden shrink-0 w-[25%] bg-blue-800 ">
                                     <div className=" font-semibold text-[15px]">
-
-
-                                        {!showDropdown && (
+                                        <div className=" text-blue-800  cursor-pointer ml-[170px]" onClick={handlePlusSPlantSFacClick}>
+                                            <BsThreeDots className="font-lighter text-[20px] hover:text-green-50"/>
+                                        </div>
+                                        <div className={`dropdown ${showDropDownSPlantSFac ? 'visible' : 'hidden'}`} >
+                                            <p className="m-0 text-white whitespace-nowrap mt-3 cursor-pointer ml-[90px]" onClick={() => setShowDeleteModal(true)}><MdDeleteOutline/></p>
+                                            {showDeleteModal ? (
                                             <>
-                                            <p className="m-0 text-white whitespace-nowrap mt-3">{`S - ADMIN`}</p>
-                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S - Adminstration`}</p>
+                                            <div className="">
+                                                <div
+                                                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                                >
+                                                <div className=" my-6 mx-auto">
+
+                                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-green-50 outline-none focus:outline-none">
+                                                
+                                                    <div className="flex items-start justify-between p-5 ">
+                                                        <h3 className="text-3xl font-semibold text-black">
+                                                        Delete 
+                                                        </h3>
+                                                        <button
+                                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                        onClick={() => setShowDeleteModal(false)}
+                                                        >
+                                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                            ×
+                                                        </span>
+                                                        </button>
+                                                    </div>
+                                                    
+                                                    <div className="relative p-6 flex-auto">
+                                                        <p className="my-4 text-blueGray-500 text-lg leading-relaxed text-black">
+                                                        Are you sure you want to delete?
+                                                        </p>
+                                                    </div>
+                                    
+                                                    <div className="flex items-center justify-end p-6">
+                                                        <button
+                                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-10" 
+                                                        type="button"
+                                                        onClick={() => setShowDeleteModal(false)}
+                                                        >
+                                                        No
+                                                        </button>
+                                                        <button
+                                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                        onClick={() => setShowDeleteModal(false)}
+                                                        >
+                                                        Yes
+                                                        </button>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                                </div>
+                                            </>
+                                            ) : null}
+
+                                        </div>
+                                        {!showDropDownSPlantSFac && (
+                                            <>
+                                            <p className="m-0 text-white whitespace-nowrap ">{`S - PLANT`}</p>
+                                            <p className="m-0 text-white whitespace-nowrap mb-3">{`S Facilities`}</p>
                                             </>
                                         )}
                                     </div>
