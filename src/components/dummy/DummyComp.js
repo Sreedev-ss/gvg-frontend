f061029c12de5a1dd57bc03d2a9abc3bfa822015
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-20 17:33:01 +0530

    edit and delete

--- src/components/dummy/DummyComp.js
+++ src/components/dummy/DummyComp.js
@@ -3,8 +3,9 @@ import { instance } from '../../api';
 import { Link } from 'react-router-dom';
 import { FaArrowLeft } from "react-icons/fa";
 import { CiCirclePlus } from "react-icons/ci";
-
-
+import { BsThreeDots } from "react-icons/bs";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 const DummyComp = () => {
     const [selectedItems, setSelectedItems] = useState([]);
@@ -13,6 +14,16 @@ const DummyComp = () => {
     const [grandparentName, setGrandparentName] = useState('');
     const [showAddModal, setShowAddModal] = useState(false);
     const [selectedSystem, setSelectedSystem] = useState('Primary');
+    const [showDropDownSPlantSFac, setShowDropDownSPlantSFac] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showEditSPlantSFacModal, setShowEditSPlantSFacModal] = useState(false);
+    const [selectedEditSystem, setSelectedEditSystem] = useState('Primary');
+
+    
+    const handlePlusSPlantSFacClick = () => {
+        console.log("ssss");
+        setShowDropDownSPlantSFac(!showDropDownSPlantSFac)
+      }
 
     useEffect(() => {
         // Fetch initial data when the component mounts
@@ -56,6 +67,10 @@ const DummyComp = () => {
 
     const handleSystemChange = (e) => {
         setSelectedSystem(e.target.value);
+    };
+
+    const handleSystemEditChange = (e) => {
+        setSelectedEditSystem(e.target.value);
     };
 
     return (
@@ -116,8 +131,8 @@ const DummyComp = () => {
                                             </label>
                                             <input
                                             type="text"
-                                            id="location"
-                                            name="location"
+                                            id="name"
+                                            name="name"
                                             //value={location}
                                             //onChange={(e) => setName(e.target.value)}
                                             className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[735px] h-[35px] text-black"
@@ -137,19 +152,6 @@ const DummyComp = () => {
                                             />
                                         </div>
                                         
-                                        {/* <div className="mb-4 mt-2">
-                                            <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-690px]">
-                                            System:
-                                            </label>
-                                            <input
-                                            type="text"
-                                            id="system"
-                                            name="system"
-                                            //value={system}
-                                            //onChange={(e) => setName(e.target.value)}
-                                            className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[735px] h-[35px]"
-                                            />
-                                        </div> */}
 
                                         <div className="mb-4 mt-2">
                                             <label htmlFor="system" className="block text-sm font-medium text-gray-700 ml-[-690px]">
@@ -167,11 +169,7 @@ const DummyComp = () => {
                                                 <option value="primary">Primary</option>
                                                 {/* <option value="primary">Primary</option> */}
                                             </select>
-                                            
-                                           
                                         </div>
-
-                                        
                         
                                         <div className="flex items-center justify-end p-6">
                                             <button
@@ -199,12 +197,169 @@ const DummyComp = () => {
 
                                 <div className="flex flex-wrap justify-center items-center gap-4">
                                     {drillDownData.map((item) => (
-                                        <div key={item._id} onClick={() => handleItemClick(item._id, item.level)} className=" rounded-6xl bg-cornflowerblue h-[215px] overflow-hidden  shrink-0 mt-2">
-                                            <div className="font-light flex justify-center items-center bg-blue-800 rounded-xl w-[300px] h-[150px] text-[15px] mt-3">
-                                                <div className=" font-semibold text-[15px] overflow:hidden">
+                                        <div key={item._id}  className=" rounded-6xl bg-cornflowerblue h-[215px] overflow-hidden  shrink-0 mt-2">
+                                            <div className=" font-light relative flex justify-center items-center bg-blue-800 rounded-xl w-[300px] h-[150px] text-[15px] mt-3">
+                                                <div className="absolute top-0 right-0 p-2 text-red-800  cursor-pointer" onClick={handlePlusSPlantSFacClick}>
+                                                    <BsThreeDots className="font-lighter text-[20px] hover:text-green-50"/>
+                                                </div>
+                                                <div className={`dropdown ${showDropDownSPlantSFac ? 'visible' : 'hidden'}`} >
+                                            <p className="m-0 text-white whitespace-nowrap mt-3 cursor-pointer " onClick={() => setShowDeleteModal(true)}><MdDeleteOutline/></p>
+                                            {showDeleteModal ? (
+                                            <>
+                                            <div className="">
+                                                <div
+                                                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                                >
+                                                <div className=" my-6 mx-auto">
+
+                                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-green-50 outline-none focus:outline-none">
+                                                
+                                                    <div className="flex items-start justify-between p-5 ">
+                                                        <h3 className="text-3xl font-semibold text-black">
+                                                        Delete 
+                                                        </h3>
+                                                        <button
+                                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                        onClick={() => setShowDeleteModal(false)}
+                                                        >
+                                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                            ×
+                                                        </span>
+                                                        </button>
+                                                    </div>
+                                                    
+                                                    <div className="relative p-6 flex-auto">
+                                                        <p className="my-4 text-blueGray-500 text-lg leading-relaxed text-black">
+                                                        Are you sure you want to delete?
+                                                        </p>
+                                                    </div>
+                                    
+                                                    <div className="flex items-center justify-end p-6">
+                                                        <button
+                                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-10" 
+                                                        type="button"
+                                                        onClick={() => setShowDeleteModal(false)}
+                                                        >
+                                                        No
+                                                        </button>
+                                                        <button
+                                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                        onClick={() => setShowDeleteModal(false)}
+                                                        >
+                                                        Yes
+                                                        </button>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                                </div>
+                                            </>
+                                            ) : null}
+
+                                        <p className="m-0 text-white whitespace-nowrap mt-3 cursor-pointer" onClick={() => setShowEditSPlantSFacModal(true)}><CiEdit/></p>
+                                        {showEditSPlantSFacModal ? (
+                                            <>
+                                            <div className="bg-slate-900">
+                                                <div
+                                                className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                                                >
+                                                <div className="relative my-6 mx-auto w-[800px]">
+
+                                                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-green-50 outline-none focus:outline-none">
+                                                
+                                                    <div className="flex items-start justify-between p-5 ">
+                                                        <h3 className="text-3xl font-semibold text-black">
+                                                        Edit 
+                                                        </h3>
+                                                        <button
+                                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                                        onClick={() => setShowEditSPlantSFacModal(false)}
+                                                        >
+                                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                                            ×
+                                                        </span>
+                                                        </button>
+                                                    </div>
+                                                    <div className="mb-4">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-680px]">
+                                                        name:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="name"
+                                                        name="name"
+                                                        //value={location}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4 mt-2">
+                                                        <label htmlFor="name" className="block text-sm font-medium text-gray-700 ml-[-660px]">
+                                                        Description:
+                                                        </label>
+                                                        <input
+                                                        type="text"
+                                                        id="description"
+                                                        name="description"
+                                                        //value={description}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4 mt-2">
+                                                        <label htmlFor="system" className="block text-sm font-medium text-gray-700 ml-[-690px]">
+                                                        System:
+                                                        </label>
+                                                        <select
+                                                        //type="text"
+                                                        id="system"
+                                                        name="system"
+                                                        value={selectedEditSystem}
+                                                        onChange={handleSystemEditChange}
+                                                        //value={system}
+                                                        //onChange={(e) => setName(e.target.value)}
+                                                        className="mt-1 ml-[-10px] p-2 border border-gray-300 rounded-md w-[740px] h-[35px]"
+                                                        >
+                                                        <option value="primary">Primary</option>
+                                                        </select>
+                                                    </div>
+                                                   
+                                                    
+                                                    <div className="flex items-center justify-end p-6">
+                                                        <button
+                                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 h-10" 
+                                                        type="button"
+                                                        onClick={() => setShowEditSPlantSFacModal(false)}
+                                                        >
+                                                        Cancel
+                                                        </button>
+                                                        <button
+                                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                        onClick={() => setShowEditSPlantSFacModal(false)}
+                                                        >
+                                                        Update
+                                                        </button>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                                                </div>
+                                            </>
+                                            ) : null}
+
+                                        </div>
+                                                {!showDropDownSPlantSFac && (
+                                                    <>
+                                                <div className=" font-semibold text-[15px] overflow:hidden" onClick={() => handleItemClick(item._id, item.level)}>
                                                     <p className="m-0 text-white mt-3 ">{item.name}</p>
                                                     <p className="m-0 text-white whitespace-nowrap mb-3 overflow-ellipsis"  style={{ whiteSpace: 'pre-line', wordWrap: 'break-word' }}>{item.description}</p>
                                                 </div>
+                                                </>
+                                                    )}
                                                 {/* <p className="m-0 text-white ">BULID</p>
                                     <p className="m-0 text-white mb-3 ">Bulidings</p> */}
                                             </div>
