ab8ff5b66b07b7d3bf22819788a694a7aa194fea
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-20 13:14:10 +0530

    ui fixing

--- src/components/dummy/DummyComp.js
+++ src/components/dummy/DummyComp.js
@@ -56,7 +56,7 @@ const DummyComp = () => {
                     <FaArrowLeft className="text-slate-500 font-lighter text-[20px]" />
                 </Link>
             </div>
-            <div className=" w-[95%] h-[92%] bg-green-50 m-6">
+            <div className=" w-[95%] h-[90%] bg-[#ebf5f4] m-6 rounded-2xl" style={{ border: '2px solid rgb(65,73,115)'}}>
                 <div className='m-15 '>
                     {grandparentName && grandparentName?.name?.length === 1 ? (
                         <b className="text-2xl cursor-pointer flex items-center justify-center" onClick={() => fetchData(grandparentName?.parent)}>
@@ -68,12 +68,12 @@ const DummyComp = () => {
                         </b>
                     )}
                     <div className='flex items-center justify-center m-16 mt-5'>
-                        <div className="rounded-6xl  py-2 px-[15px] box-border ml-[-66px] bg-green-200 rounded-2xl w-[20%]" onClick={() => fetchData(parentName?.parent)}>
+                        <div className="rounded-6xl  py-2 px-[15px] box-border ml-[-66px] bg-[rgb(215,235,230)] rounded-2xl w-[20%]" style={{ border: '2px solid rgb(77,164,164)'}} onClick={() => fetchData(parentName?.parent)}>
                             <p className="m-0 text-black mt-2 justify-center items-center flex font-semibold">{parentName?.name?.length !== 1 && parentName?.name}</p>
                             <p className="m-0 text-black justify-center items-center flex font-semibold">{parentName?.description}</p>
                         </div>
                         <div className='absolute mt-0 left-0 top-60'>
-                            <div className="text-center text-xl text-white bg-green-200 rounded-2xl ml-[310px] mt-[30px] w-[131vh] h-[64vh]">
+                            <div className="text-center text-xl text-white bg-[rgb(215,235,230)] rounded-2xl ml-[310px] mt-[30px] w-[159vh] Min-h-[58vh]" style={{ border: '2px solid rgb(77,164,164)', overflowY: 'auto', maxHeight: '58vh'  }}>
                                 <div className="flex flex-wrap">
                                     {drillDownData.map((item) => (
                                         <div key={item._id} onClick={() => handleItemClick(item._id, item.level)} className=" rounded-6xl bg-cornflowerblue h-[215px] overflow-hidden shrink-0 ml-[5%] mt-2">
