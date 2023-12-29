d57f955eef1792e4a94c6d81ba81c596c996288a
Sreedev-ss <sreedevkply2004@gmail.com>
2023-12-29 15:01:42 +0530

    ui fix

--- src/pages/dashboard/AdminDashboard.js
+++ src/pages/dashboard/AdminDashboard.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import AdminDashboardComp from '../../components/adminDashboardComp/AdminDashboardComp';
 import AdminSidebar from '../../components/adminSidebar/AdminSidebar';
+import { useHierarchy } from '../../context/HierarchyContext';
 
 const AdminDashboard = () => {
+  const { updatePath, updateParent, updateLevel, selectItem } = useHierarchy();
+  useEffect(() => {
+    selectItem(null)
+    updatePath([])
+    updateParent(null)
+    updateLevel(1)
+  }, [])
   return (
     <div className="flex h-[96vh] overflow-y-hidden">
-            <AdminSidebar />
+      <AdminSidebar />
 
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <Navbar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Navbar />
 
-                <div className="flex-1  overflow-hidden">
-                    <AdminDashboardComp />
-                </div>
-            </div>
+        <div className="flex-1  overflow-hidden">
+          <AdminDashboardComp />
         </div>
+      </div>
+    </div>
   )
 }
 
