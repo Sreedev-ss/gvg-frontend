4c3006375a443d688bad0b8b1f561fd7b4dd082e
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-29 12:09:35 +0530

    height

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
     return (
-        <nav className="bg-white text-black rounded-lg h-12 mb-3 ">
+        <nav className="bg-white text-black rounded-lg h-12 mb-3 w-[50%] flex items-center ml-[25%]">
             <div className='text-lg text-darkslategray-100 font-nunito-sans flex '>
             <div className="rounded-[14px] bg-white shadow-[0px_6px_58px_rgba(196,_203,_214,_0.1)]" />
             <CiSearch className='m-4'/>
