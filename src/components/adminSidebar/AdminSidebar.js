1e3592f7d099bacc0ad99422eb0144c79bc998d7
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-29 22:08:01 +0530

    sidbar ui

--- src/components/adminSidebar/AdminSidebar.js
+++ src/components/adminSidebar/AdminSidebar.js
@@ -179,7 +179,7 @@ const AdminSidebar = () => {
                     </button>
                 </div>
                 <div>
-                    <div className="mt-16  text-zinc-900 text-[14px] cursor-pointer flex items-center justify-center">
+                    <div className="mt-36 text-zinc-900 text-[14px] cursor-pointer flex items-center justify-center">
                         <IoMdSettings style={{ marginRight: '10px' }} />
                         Settings
                     </div>
