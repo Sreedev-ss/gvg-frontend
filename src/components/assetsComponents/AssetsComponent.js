e0989f4625593752d6fe5a7c6e1f0be61eb71984
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-29 21:49:27 +0530

    assets

--- src/components/assetsComponents/AssetsComponent.js
+++ src/components/assetsComponents/AssetsComponent.js
@@ -4,13 +4,13 @@ import { FaArrowLeft } from "react-icons/fa";
 
 const AssetsComponenet = () => {
     return (
-        <div className="bg-white p-4 h-[87.1vh] rounded-lg shadow-md">
+        <div className="bg-white p-4 h-[89.4vh] rounded-lg shadow-md">
             <div className="top-0 left-0 m-4 cursor-pointer">
                 <Link to='/'>
                     <FaArrowLeft className="text-slate-500 font-lighter text-[20px]"/>
                 </Link>
             </div>
-            <div className="h-[90%] bg-[rgb(245,249,255)] m-6 rounded-2xl" style={{ border: '2px solid rgb(17,110,255)'}}>
+            <div className="h-[78%] max-h-[80%] bg-[rgb(245,249,255)] m-6 rounded-2xl" style={{ border: '2px solid rgb(17,110,255)'}}>
                 <div className='m-15'>
                     <b className="text-4xl cursor-pointer flex items-center justify-center">
                         Assets
@@ -20,7 +20,7 @@ const AssetsComponenet = () => {
                             <div className="text-center text-xl text-white ">
                             <div className="flex">
                            
-                                    <div className="rounded-6xl bg-cornflowerblue h-[215px] overflow-hidden shrink-0 ml-[1%] mt-2">
+                                    <div className="rounded-6xl bg-cornflowerblue overflow-hidden shrink-0 ml-[1%] mt-2">
                                         <Link to='/asset'>
                                             <div className="font-semibold bg-[rgb(55,115,202)] rounded-xl w-[200px] h-[100px] text-[15px] mt-14 flex items-center justify-center">
                                                 <p className="m-0 text-white mt-[-20px] ">East</p>
