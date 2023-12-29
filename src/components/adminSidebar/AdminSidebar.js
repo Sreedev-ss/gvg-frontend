b8d83d28fd72258ff5d815192f8c44ec0dd5bdce
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-12-29 12:34:43 +0530

    export

--- src/components/adminSidebar/AdminSidebar.js
+++ src/components/adminSidebar/AdminSidebar.js
@@ -9,13 +9,63 @@ import { IoMdSettings } from "react-icons/io";
 import { IoIosLogOut } from "react-icons/io";
 import { HiBuildingLibrary } from "react-icons/hi2";
 import { SiGoogleanalytics } from "react-icons/si";
+import Papa from 'papaparse';
+import { instance } from '../../api';
+import { useHierarchy } from '../../context/HierarchyContext';
 
 const AdminSidebar = () => {
+    const { hierarchicalPath, selectItem, parentid, level } = useHierarchy();
     const navigate = useNavigate();
 
     const handleLogout = () => {
         navigate('/authentication/login')
     }
+
+    const handleExport = () => {
+        const fields = ['name', 'description', 'system', 'parent', 'level'];
+
+        try {
+            const exportedData = [];
+            instance.get(`/assets/drill-asset/${parentid}`).then((res) => {
+                res.data.forEach(async (item) => {
+                    await flattenHierarchy(item, exportedData);
+                });
+                const csv = Papa.unparse({
+                    fields: fields,
+                    data: exportedData,
+                });
+
+                const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+                const link = document.createElement('a');
+                const url = URL.createObjectURL(blob);
+
+                link.href = url;
+                link.setAttribute('download', 'exported_asset_data.csv');
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }).catch(err => {
+                console.log(err)
+            })
+            function flattenHierarchy(item, exportedData) {
+                exportedData.push({
+                    name: item.name,
+                    description: item.description,
+                    system: item.system,
+                    parent: item.parent,
+                    level: item.level
+                });
+
+                if (item.children && item.children.length > 0) {
+                    item.children.forEach((child) => {
+                        flattenHierarchy(child, exportedData);
+                    });
+                }
+            }
+        } catch (error) {
+            console.error('Error exporting data:', error);
+        }
+    };
 
     return (
         <aside className="bg-white p-4 text-white rounded-lg mr-4 w-60">
@@ -58,7 +108,7 @@ const AdminSidebar = () => {
                         </button>
                     </div>
                     <button className="rounded-31xl flex items-center justify-center py-2.5 px-5 gap-[9px] text-center text-[14px] text-white cursor-pointer rounded  bg-[rgb(254,132,183)] border-[rgb(254,132,183)] mt-4"
-                        //onClick={handleExport}
+                        onClick={handleExport}
                     >
                         <CiExport />Export
                     </button>
